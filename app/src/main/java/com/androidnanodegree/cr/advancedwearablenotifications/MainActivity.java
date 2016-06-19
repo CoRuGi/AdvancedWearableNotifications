2a8de77d439d09c6ba44c1fa3367cb2db14ab611
CoRuGi <CR.Github.n6qc4@gmail.com>
2016-06-19 18:18:30 +0200

    feat: Add a button that executes the Standard Notification

--- app/src/main/java/com/androidnanodegree/cr/advancedwearablenotifications/MainActivity.java
+++ app/src/main/java/com/androidnanodegree/cr/advancedwearablenotifications/MainActivity.java
@@ -2,6 +2,9 @@ package com.androidnanodegree.cr.advancedwearablenotifications;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.androidnanodegree.cr.advancedwearablenotifications.util.StandardNotification;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void CreateStandardNotification(View view){
+        StandardNotification standardNotification = new StandardNotification(this);
+        standardNotification.show();
     }
 }
